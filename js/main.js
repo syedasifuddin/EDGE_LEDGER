e7080d778bebdb3fbd781353f7a8e15dba635c2e
Syed Asif Uddin <syed.uddin@lsz.ads.valuelabs.net>
2019-02-28 19:42:16 +0530

    edge ledger| added google maps| smooth scrolling "

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function initMap() {
+  // The location of Uluru
+  var uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 4,
+    center: uluru
+  });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+}
+
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
