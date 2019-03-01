7134e7a2646318b97e8f21558933b4509573e402
Syed Asif Uddin <syed.uddin@lsz.ads.valuelabs.net>
2019-03-01 17:45:55 +0530

    completed edge ledger"

--- js/main.js
+++ js/main.js
@@ -3,13 +3,23 @@ function initMap() {
   var uluru = { lat: -25.344, lng: 131.036 };
   // The map, centered at Uluru
   var map = new google.maps.Map(document.querySelector(".map"), {
-    zoom: 4,
+    zoom: 14,
     center: uluru
   });
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({ position: uluru, map: map });
 }
 
+// Sticky Menu Background
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
 $("#navbar a, .btn").on("click", function(event) {
   if (this.hash !== "") {
     event.preventDefault();
